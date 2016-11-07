a2c459edc17fb31d707e8ef979ed29cfacc458fe
ashlopez <YOUR-ashleylopezglobal@gmail.com>
2016-11-06 16:35:39 -0800

    add step 3

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 
 $( document ).ready(function() {
+
+  /* step 1 login*/
  		var  step_1 = $.post( "http://challenge.code2040.org/api/register", 
  			{ token: "53a950ad6b0a3f43d9dd1fd2364c3d32", github: "https://github.com/ashlopeztech/api_challenge1" })
   				.done(function( data ) {
@@ -9,7 +11,7 @@ $( document ).ready(function() {
   		});
 
 
-  
+     step 2 reverse string
   		var step_2 = $.post( "http://challenge.code2040.org/api/reverse", { token: "53a950ad6b0a3f43d9dd1fd2364c3d32" })
   		.done(function( data ) {
      		 alert( " alert 2: Data Loaded: " + data );
@@ -36,29 +38,66 @@ $( document ).ready(function() {
 
           var global_data= reverseString(data);
           alert("alert5"+global_data);
-
+          
 
           var  step2_half = $.post( "http://challenge.code2040.org/api/reverse/validate", 
         { token: "53a950ad6b0a3f43d9dd1fd2364c3d32", string: global_data })
           .done(function( data ) {
             alert( "alert 6"+"Data Loaded: " + data );
+          });
+
+
+      });
+           
+        
+        
+       var  step_3 = $.post( "http://challenge.code2040.org/api/haystack", 
+      { token: "53a950ad6b0a3f43d9dd1fd2364c3d32" })
+          .done(function( data ) {
+            alert( "Data Loaded: " + data );
+
+
+            console.log(data.needle);
+            console.log(data.haystack);
+
+            var a= data.haystack;
+            var b = data.needle;
+            var c = a.indexOf(data.needle);
+
+            var  step3_half = $.post( "http://challenge.code2040.org/api/haystack/validate", 
+      { token: "53a950ad6b0a3f43d9dd1fd2364c3d32", needle:c })
+          .done(function( data ) {
+            alert( "step 3.5"+"Data Loaded: " + data );
       });
 
+      }); */
+
+ var  step_4 = $.post( "http://challenge.code2040.org/api/prefix", 
+      { token: "53a950ad6b0a3f43d9dd1fd2364c3d32" })
+          .done(function( data ) {
+            alert( "Data Loaded: " + data );
+
+            console.log(data.prefix);
+            console.log(data.array);
+
+            var a = data.prefix
+            var myArray = data.array
+             
+            for(var i=0; i < myArray.length; i++) {
+                   
+                  if(myArray[i]== [i].includes(a) ){
+
+                    myArray.splice([i]);
+
+                  }
+                  else{
+                    console.log("dontremove")
+                  }
+}
 
 
-          });
-           
-          
 
-       
-
-
-     		 /*var post_reversed = $.post( "http://challenge.code2040.org/api/reverse/validate", 
- 				    { token: "53a950ad6b0a3f43d9dd1fd2364c3d32", string: reverseString(data) })
-  				 .done(function( data ) {
-     				alert( "Data Loaded: " + data );
-     			};*/
-	 
+    });
     	
 
 
